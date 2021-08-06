fc1f80cb84442bf545d3fc19fadfc8af9b8a23e8
SumitNegi7 <sumitnegi2142@gmail.com>
2021-08-06 21:14:06 +0530

    Add comments to test case

--- src/test.e2e.js
+++ src/test.e2e.js
@@ -4,10 +4,10 @@ test.describe("Screenshot Testing", () => {
   test("compares screenshots for desktop, tablet and mobile screen", async ({ page }) => {
     await page.goto("https://www.manufacanalytics.com/", {
       waitUntil: "networkidle",
-    });
+    }); // #ref https://www.manufacanalytics.com/ uses babylon.js 3D cube which is constantly spinning so we don't want to take its snapshot so hiding canvas which contains the cube
     await page.waitForSelector("canvas", { state: "visible" });
     await page.evaluate(() => {
-      (document.querySelector("canvas")).style.visibility = "hidden";
+      (document.querySelector("canvas")).style.visibility = "hidden"; // Not hiding canvas takes screenshots in firefox but it is a blank page
     });
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot("snap.png", { threshold: 0.2 });
   });
